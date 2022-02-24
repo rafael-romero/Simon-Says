ce433dceef891a207cb95de4dc0cf4f9e7766e3e
Rafael <95598903+rafael-romero@users.noreply.github.com>
2022-02-23 22:49:59 -0300

    agrega funcionalidad al juego

--- main.js
+++ main.js
@@ -39,3 +39,55 @@ $botonOk.onclick = function(event){
 
     event.preventDefault();
 }
+
+const coloresHover = {
+    green: "rgb(0, 179, 0)",
+    red: "rgb(255, 51, 51)",
+    yellow: "rgb(255, 255, 102)",
+    blue: "rgb(51, 51, 255)"
+}
+
+const $botonJugar = document.querySelector("#btn-jugar");
+$botonJugar.onclick = function(){
+    arregloJugadasPc = [];
+    mostrarUOcultarElementos("btn-jugar");
+    //document.querySelector("#btn-jugar").classList.add("oculto");
+    reiniciarContadorDeRondas();
+    mostrarUOcultarElementos("rondas");
+    //document.querySelector("#rondas").classList.remove("oculto");
+    desarrollarJuego();
+    return false;
+}
+
+const $botongreen = document.querySelector("#boton-green");
+const $sonidoBtngreen = document.querySelector("#sonido-btn-green");
+$botongreen.onclick = function(){
+    $sonidoBtngreen.play();
+    arregloJugadasUsuario.push("green");
+    compararSecuencias(arregloJugadasPc, arregloJugadasUsuario);
+}
+
+const $botonred = document.querySelector("#boton-red");
+const $sonidoBtnred = document.querySelector("#sonido-btn-red");
+$botonred.onclick = function(){
+    $sonidoBtnred.play();
+    arregloJugadasUsuario.push("red");
+    compararSecuencias(arregloJugadasPc, arregloJugadasUsuario);
+}
+
+const $botonyellow = document.querySelector("#boton-yellow");
+const $sonidoBtnyellow = document.querySelector("#sonido-btn-yellow");
+$botonyellow.onclick = function(){
+    $sonidoBtnyellow.play();
+    arregloJugadasUsuario.push("yellow");
+    compararSecuencias(arregloJugadasPc, arregloJugadasUsuario);
+}
+
+const $botonblue = document.querySelector("#boton-blue");
+const $sonidoBtnblue = document.querySelector("#sonido-btn-blue");
+$botonblue.onclick = function(){
+    $sonidoBtnblue.play();
+    arregloJugadasUsuario.push("blue");
+    compararSecuencias(arregloJugadasPc, arregloJugadasUsuario);
+}
+
