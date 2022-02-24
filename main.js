d30c288b8b62c62f2bb64302758c1d6ab0a72b2c
Rafael <95598903+rafael-romero@users.noreply.github.com>
2022-02-23 22:50:04 -0300

    Update main.js

--- main.js
+++ main.js
@@ -11,33 +11,161 @@ function validarNombre(nombre){
     return "";
 }
 
-
+let nombreDeUsuario = "";
 const $botonOk = document.querySelector("#boton-ok");
 $botonOk.onclick = function(event){
+    document.querySelector("#btn-jugar").classList.add("oculto");
     const $nombre = document.querySelector("#nombre-usuario").value
     const errorNombre = validarNombre($nombre);
-    const $labelNombreUsuario = document.querySelector("#nombre-de-usuario");
-    const mensajeError = document.createElement("strong");
+    const mensajeError = document.querySelector("#advertencia-nombre");
     if (errorNombre){
-        //$labelNombreUsuario.classList.add("error");
-        
-        
         document.querySelector("#nombre-usuario").classList.add("error");
-        
-        if(!$labelNombreUsuario.contains(mensajeError)){
-            mensajeError.textContent = errorNombre;
-            $labelNombreUsuario.appendChild(mensajeError);
-        }    
+        mensajeError.textContent = errorNombre;
+        mensajeError.classList.remove("oculto");
     }else {
-        if($labelNombreUsuario.contains(mensajeError)){
-           document.querySelector("#nombre-usuario").classList.remove("error");
-            $labelNombreUsuario.removeChild(mensajeError);
-            //deberia agregar aca el nombre de usuario al localstorage
+        mensajeError.classList.add("oculto");
+        document.querySelector("#nombre-usuario").classList.remove("error");
+    }
+    nombreDeUsuario = $nombre;
+    document.querySelector("#nombre").textContent = nombreDeUsuario;
+    if(!errorNombre){
+        alert(`Bienvenidx Estimadx ${$nombre}...jugamos???`);
+        //document.querySelector("#btn-jugar").classList.remove("oculto");
+        //hace lo mismo que la linea de abajo, la dejo hasta estar seguro
+        mostrarUOcultarElementos("btn-jugar");
+    }
+    event.preventDefault();
+}
+function reiniciarContadorDeRondas(){
+    return document.querySelector("#cantidad-rondas").textContent = "0";
+}
+
+function activarBotones(){
+    $botongreen.disabled = false;
+    $botonred.disabled = false;
+    $botonyellow.disabled = false;
+    $botonblue.disabled = false;
+}
+
+function desactivarBotones(){
+    $botongreen.disabled = true;
+    $botonred.disabled = true;
+    $botonyellow.disabled = true;
+    $botonblue.disabled = true;
+}
+
+// function cambiarColorBackground($botonColor , color){
+//     $botonColor.style.backgroundColor = color;
+// }
+
+// function cambiarScalaANormalBoton($botonColor){
+//     $botonColor.style.transform.scale = "1";
+// }
+
+function borrarClase($botonColor, color){
+    $botonColor.classList.remove(`marcado-${color}`)
+}
+
+function mostrarSecuenciaPC(arreglo){
+    arreglo.forEach(function(color){
+        document.querySelector(`#sonido-btn-${color}`).play();
+        const $botonColor = document.querySelector(`#boton-${color}`);
+        $botonColor.classList.add(`marcado-${color}`);
+        // $botonColor.style.backgroundColor = `coloresHover[color]`;
+        // $botonColor.style.transform.scale = "1.2";
+        setTimeout(borrarClase($botonColor, color), 2000)
+
+        // setTimeout(cambiarScalaANormalBoton($botonColor), 2000);
+        // setTimeout(cambiarColorBackground($botonColor, color), 2000);
+        
+        //mostrar el boton con un color distinto y hacer sonido
+        // document.querySelector(`#boton-${color}`).style.backgroundColor = coloresHover[color]
+        // document.querySelector(`#boton-${color}`).style.transform = scale(1.2);
+        
+        //const $sonido =  document.querySelector(`#sonido-btn-${color}`);
+        //$sonido.play();
+        
+    });
+}
+
+function generarSecuenciaPC(){
+    const numeroAleatorio= Math.floor(Math.random() * 4) + 1;
+    arregloJugadasPc.push(colores[numeroAleatorio]);
+    mostrarSecuenciaPC(arregloJugadasPc);
+}
+
+function compararSecuencias(arregloPc, arregloUsuario){
+    for(let i = 0; i < arregloUsuario.length; i++){
+        if(arregloPc[i] !== arregloUsuario[i]){
+            //return terminoJuego = true;
+            desactivarBotones();
+            finalizarJuego();
         }
     }
+    if(arregloPc.length === arregloUsuario.length){
+        desactivarBotones();
+        desarrollarJuego();
+    }
+}
+
+function mostrarUOcultarElementos(elemento){
+    document.querySelector(`#${elemento}`).classList.toggle("oculto");
+}
 
 
-    event.preventDefault();
+const $botonNO = document.querySelector("#btn-no");
+$botonNO.onclick = function(){
+    alert("Gracias, esperamos el juego haya sido de su agrado!!!");
+    return false;
+}
+
+const $botonSi = document.querySelector("#btn-si");
+$botonSi.onclick = function(){
+    mostrarUOcultarElementos("finalizado-juego");
+    mostrarUOcultarElementos("conjunto-botones");
+    mostrarUOcultarElementos("btn-jugar");
+    // document.querySelector("#finalizado-juego").classList.add("oculto");
+    // document.querySelector("#conjunto-botones").classList.remove("oculto");
+    // document.querySelector("#btn-jugar").classList.remove("oculto");
+    return false;
+}
+
+function mostrarFinalizadoJuego(){
+    document.querySelector("#n-rondas").textContent = String(cantidadDeRondas - 1); 
+    mostrarUOcultarElementos("finalizado-juego");
+    //document.querySelector("#finalizado-juego").classList.remove("oculto");
+}
+
+function finalizarJuego(){
+    const $sonidoFinalizadoJuego = document.querySelector("#audio-finalizado");
+    $sonidoFinalizadoJuego.play();
+    mostrarFinalizadoJuego()
+    mostrarUOcultarElementos("conjunto-botones");    
+    //document.querySelector("#conjunto-botones").classList.add("oculto");
+}
+
+let arregloJugadasPc = [];
+let arregloJugadasUsuario = [];
+let cantidadDeRondas = 0;
+//let terminoJuego = false;
+
+function desarrollarJuego(){
+    //while(terminoJuego !== true){
+        cantidadDeRondas++;
+        document.querySelector("#cantidad-rondas").textContent = String(cantidadDeRondas);
+        generarSecuenciaPC();
+        alert("Es su turno de jugar!!!")
+        arregloJugadasUsuario = [];
+        activarBotones();
+        //terminoJuego = compararSecuencias(arregloJugadasPc, arregloJugadasUsuario);
+    //}
+}
+
+const colores = {
+    1: "green",
+    2: "red",
+    3: "yellow",
+    4: "blue"
 }
 
 const coloresHover = {
